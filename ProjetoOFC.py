f65783375d59a57cf35b565664a32266fc068296
pvbrto <paul.brto@gmail.com>
2023-03-08 18:49:47 -0300

    Fix

--- ProjetoOFC.py
+++ ProjetoOFC.py
@@ -1,13 +1,8 @@
 import time
-print("=======================================================")
 print("=============NL1A - Ondas Eletromagnéticas=============")
-print("=======================================================")
 time.sleep(3)
 print()
-print()
-print("=======================================================")
 print("==================Sumário e Objetivos==================")
-print("=======================================================")
 print()
 print("Esse programa tem como objetivo calcular os valores de Em, Bm, I, f, lambda, k e omega, a partir de um valor conhecido.")
 print()
@@ -26,24 +21,21 @@ print("como transformadores e motores elétricos. As ondas magnéticas têm vár
 print("incluindo a comunicação sem fio, a produção de imagens médicas, a inspeção de materiais e a geração ")
 print("de energia elétrica.")
 print()
-print()
 time.sleep(3)
-print("=======================================================")
 print("==================Integrantes do Grupo=================")
-print("=======================================================")
-print("- Livia Miyabara\n- Luiggi\n- Marcio Forner\n- Paulo Brito")
 print()
+print("- Livia Miyabara\n- Luiggi\n- Marcio Forner\n- Paulo Brito")
 print()
 time.sleep(3)
 pi = 3.1416
 C = 300000000
 micro0 = 4 * pi * 10**-7
 print("======================CONSTANTES=======================")
-print(f"pi = {pi}\nVelocidade da Luz(C) = {C:.2e}\nmicro0 = {micro0:.2e}")
 print()
+print(f"pi = {pi}\nVelocidade da Luz(C) = {C:.2e}\nConstanteMagnética(micro0) = {micro0:.2e}")
 print()
 time.sleep(3)
-print("OBS: O programa só aceita números com ponto, não com vírgula, além disso escreva todos os valores em notação científica")
+print("OBS: O programa só aceita números com ponto, não com vírgula, além disso escreva todos os valores em relação a unidade Ex. 60uT = 60*10^-6T")
 resposta = 1
 while resposta == 1:
     time.sleep(3)
@@ -57,27 +49,32 @@ while resposta == 1:
     print("===============MENU DE OPÇÕES DE CALCULO===============")
     Opcao = int(input("Escolha uma opção:\n1 - Em\n2 - Bm\n3 - I\n4 - f\n5 - Lambda\n6 - k\n7 - Omega\n=======================================================\nEscolha a opção:"))
     time.sleep(1)
+    print()
     
     if Opcao == 1:
         Em = float(input("Digite o valor de Em: "))
+        print()
         Bm = (Em)/C
         print(f"O valor de Bm é: {Bm:.2e} T")
         I = (1/(2* micro0 * C)) * (Em ** 2)
         print(f"O valor de I é: {I:.2e} W/m²")
     elif Opcao == 2:
         Bm = float(input("Digite o valor de Bm: "))
+        print()
         Em = Bm * C
         print(f"O valor de Em é: {Em:.2e} V/m")
         I = (C/(2* micro0)) * (Bm ** 2)
         print(f"O valor de I é: {I:.2e} W/m²")
     elif Opcao == 3:
         I = float(input("Digite o valor de I: "))
+        print()
         Em = (2 * micro0 * C * I)**(1/2)
         print(f"O valor de Em é: {Em:.2e} V/m")
         Bm = ((2 * micro0 * I)/C)**(1/2)
         print(f"O valor de Bm é: {Bm:.2e} T")
     elif Opcao == 4:
         f = float(input("Digite o valor de f: "))
+        print()
         lambdaa = C/f
         print(f"O valor de lambda é: {lambdaa:.2e} m")
         k = 2 * pi/lambdaa
@@ -86,6 +83,7 @@ while resposta == 1:
         print(f"O valor de omega é: {omega:.2e} rad/s")
     elif Opcao == 5:
         lambdaa = float(input("Digite o valor de lambda: "))
+        print()
         f = C/lambdaa
         print(f"O valor de f é: {f:.2e} Hz")
         k = 2 * pi/lambdaa
@@ -94,6 +92,7 @@ while resposta == 1:
         print(f"O valor de omega é: {omega:.2e} rad/s")
     elif Opcao == 6:
         k = float(input("Digite o valor de k: "))
+        print()
         lambdaa = 2 * pi/k
         print(f"O valor de lambda é: {lambdaa:.2e} m")
         f = C/lambdaa
@@ -102,6 +101,7 @@ while resposta == 1:
         print(f"O valor de omega é: {omega:.2e} rad/s")
     elif Opcao == 7:
         omega = float(input("Digite o valor de omega: "))
+        print()
         f = omega/(2 * pi)
         print(f"O valor de f é: {f:.2e} Hz")
         lambdaa = C/f
@@ -110,7 +110,6 @@ while resposta == 1:
         print(f"O valor de k é: {k:.2e} m^-1")
     else:
         print("Opção inválida!")
-    print()
     print()
     resposta = int(input("Deseja fazer um outro estudo? 1 - Sim 2 - Não: "))
     print()
